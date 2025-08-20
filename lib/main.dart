7665863d1829764e111708d2e3cf6e715161c696
seoyeon-haru <sy5792@naver.com>
2025-08-20 17:57:13 +0900

    Riverpod 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movie_app/pages/welcome/welcome_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
