a34b9d9aaeab1e591542b0873a12e64b4ab1ca5d
seoyeon-haru <sy5792@naver.com>
2025-08-22 17:33:20 +0900

    feat: DataSource 테스트 구현

---
+++ test/data/movie_data_source_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_movie_app/data/data_source/movie_data_source_impl.dart';
+import 'package:flutter_movie_app/data/dto/movie_detail_dto.dart';
+import 'package:flutter_movie_app/data/dto/movie_reponse_dto.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class FakeMovieResponseDto extends Mock implements MovieResponseDto {}
+
+class FakeMovieDetailDto extends Mock implements MovieDetailDto {}
+
+void main() {
+  late MovieDataSourceImpl dataSource;
+  late MovieResponseDto fakeResponseDto;
+  late MovieDetailDto fakeDetailDto;
+
+  setUp(() {
+    fakeResponseDto = FakeMovieResponseDto();
+    fakeDetailDto = FakeMovieDetailDto();
+    dataSource = MovieDataSourceImpl(
+      movieResponseDto: fakeResponseDto,
+      movieDetailDto: fakeDetailDto,
+    );
+  });
+
+  test('MovieDataSourceImpl : fetchNowPlaying', () async {
+    final result = await dataSource.fetchNowPlayingMovies();
+    expect(result, same(fakeResponseDto));
+  });
+
+  test('MovieDataSourceImple: fetchPopular', () async {
+    final result = await dataSource.fetchPopularMovies();
+    expect(result, same(fakeResponseDto));
+  });
+
+  test('MovieDataSourceImpl: fetchTopRated', () async {
+    final result = await dataSource.fetchTopRatedMovies();
+    expect(result, same(fakeResponseDto));
+  });
+
+  test('MovieDataSourceImpl: fetchUpComing', () async {
+    final result = await dataSource.fetchUpComingMovies();
+    expect(result, same(fakeResponseDto));
+  });
+
+  test('MovieDataSourceImpl : fetchMovieDetail', ()async{
+    final result = await dataSource.fetchMovieDetail(123);
+    expect(result, same(fakeDetailDto));
+  });
+}
